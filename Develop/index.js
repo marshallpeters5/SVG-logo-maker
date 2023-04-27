1eef4ebe11170814fc4e6ddc5e7c5be8e086b00c
Marshall <marshall.peters01@gmail.com>
2023-04-27 13:56:05 -0400

    added a fs.writeFile function to create our SVG

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const {Circle, Triangle, Square} = require('./lib/shapes.js')
+const fs = require('fs')
 
 inquirer.prompt([
   {
@@ -16,7 +17,7 @@ inquirer.prompt([
   },
   {
     type: 'input',
-    name: 'text-color',
+    name: 'textColor',
     message: 'Please enter a color for the text.'
   },
   {
@@ -26,10 +27,29 @@ inquirer.prompt([
   },
   {
     type: 'input',
-    name: 'shape-color',
+    name: 'shapeColor',
     message: 'Please enter a color for shape.'
   }
 
 ]).then((answers) => {
-  console.log(`You entered: ${answers.text}`);
-});
+  let shape = ''
+  if(answers.shape === 'circle'){
+    shape = new Circle()
+  } else if (answers.shape === 'square') {
+    shape = new Square()
+  } else {
+    shape = new Triangle()
+  }
+  shape.setColor(answers.shapeColor)
+  return `
+  <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+  ${shape.render()}
+
+  <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
+
+</svg>`
+}).then(data =>{
+  console.log(data);
+  fs.writeFileSync('logo.svg', data)
+})
